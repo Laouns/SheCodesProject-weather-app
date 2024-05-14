c4f3f17efb34689a4e88fe9de637043963401a34
Laura <laouns@gmail.com>
2024-05-13 23:40:05 -0500

    Added forecast JS and API Integration

--- script.js
+++ script.js
@@ -35,9 +35,47 @@ function getWeather(city) {
   let apiKey = "67d0faba43ba3e4a6a1f2t07194ao013";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeather);
+
+  // make api call and update forecast
+  let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrlForecast).then(displayForecast);
+}
+
+function forecastDaily(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return weekdays[date.getDay()];
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let dailyForecast = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      dailyForecast += `
+          
+        <div class="row">
+          <div class="column">
+            <h3>${forecastDaily(day.time)}</h3>
+            <img src="${day.condition.icon_url}" alt="weather-icon" />
+            <div class="temp">
+              <span class="max">${Math.round(day.temperature.maximum)}°</span>
+              <span class="min">${Math.round(day.temperature.minimum)}°</span>
+            </div>
+          </div>
+        </div>
+        `;
+    }
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = dailyForecast;
 }
 
 let search = document.getElementById("search-city");
+
 search.addEventListener("submit", function (event) {
   event.preventDefault();
   let city = document.querySelector("#input").value;
